20a1414d9214402ec96b089385956c5205ddda5a
thomasbetend <thomas.betend@yahoo.fr>
2022-12-13 10:54:36 +0100

    biblitek home page list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  like1 = 0;
+  like2 = 0;
+  
   constructor() {}
+
+  addLike1() {
+    if (this.like1 === 0) {
+      this.like1++;
+    } else {
+      this.like1--;
+    }
+  }
+
+  addLike2() {
+    if (this.like2 === 0) {
+      this.like2++;
+    } else {
+      this.like2--;
+    }
+  }
 
 }
